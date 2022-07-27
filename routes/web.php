f07c6435a7d80973f51289c73ba677f297fa7b90
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2022-07-28 01:18:55 +0200

    CollectionController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CollectionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('collections' , [CollectionController::class , 'index']);
